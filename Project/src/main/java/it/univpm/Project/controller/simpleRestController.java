5b23b76926f0b9c8d3c814c921c212124b9bdca2
CristianCingo <52571810+CristianCingo@users.noreply.github.com>
2019-11-27 13:27:51 +0100

    First commit & push

---
+++ Project/src/main/java/it/univpm/Project/controller/simpleRestController.java
@@ -0,0 +1,17 @@
+package it.univpm.Project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.univpm.Project.model.HelloWorldClass;
+
+
+
+@RestController
+public class simpleRestController {
+@GetMapping("/hello")
+public HelloWorldClass exampleMethod(@RequestParam(name="param1",defaultValue="World") String param1) {
+return new HelloWorldClass("Cristian","Cingolani");
+}
+}
