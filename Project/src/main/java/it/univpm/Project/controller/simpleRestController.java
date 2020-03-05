a120b4058b2868187f34fe69e5a6fffdde1a4374
Christian <s1084723@studenti.univpm.it>
2020-03-05 09:29:26 +0100

    Aggiunta PostMapping

--- Project/src/main/java/it/univpm/Project/controller/simpleRestController.java
+++ Project/src/main/java/it/univpm/Project/controller/simpleRestController.java
@@ -1,6 +1,8 @@
 package it.univpm.Project.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +15,9 @@ public class simpleRestController {
 @GetMapping("/hello")
 public HelloWorldClass exampleMethod(@RequestParam(name="param1",defaultValue="World") String param1) {
 return new HelloWorldClass("Cristian","Cingolani");
+}
+@PostMapping("/hello")
+public HelloWorldClass exampleMethod1(@RequestBody String body) {
+return new HelloWorldClass("Christian","Ascani");
 }
 }
