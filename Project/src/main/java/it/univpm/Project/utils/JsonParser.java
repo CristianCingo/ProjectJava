90ea02ce5e6267e8ec5d1e4eff8fd0031fa5b5fc
CristianCingo <52571810+CristianCingo@users.noreply.github.com>
2020-03-14 10:39:31 +0100

    Json Parser con Gson

--- Project/src/main/java/it/univpm/Project/utils/JsonParser.java
+++ Project/src/main/java/it/univpm/Project/utils/JsonParser.java
@@ -1,5 +1,38 @@
 package it.univpm.Project.utils;
 
-public class JsonParser {
+import it.univpm.Project.dataset.Funding;
+
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.apache.catalina.User;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class JsonParser{
+	{
+	try {
+	    // create Gson instance
+	    Gson gson = new Gson();
+
+	    // create a reader
+	    Reader reader = Files.newBufferedReader(Paths.get("dataFile.json"));
+
+	    // convert JSON array to list of users
+	    List<Funding> fundings = new Gson().fromJson(reader, new TypeToken<List<Funding>>() {}.getType());
+
+	    // print users
+	    fundings.forEach(System.out::println);
+
+	    // close reader
+	    reader.close();
+
+	} catch (Exception ex) {
+	    ex.printStackTrace();
+	}
+	}
 
 }
