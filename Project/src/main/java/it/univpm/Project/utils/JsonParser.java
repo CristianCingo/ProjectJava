e48d940fbf0ef60b2d79c969a920a5f96ce66c39
Christian <s1084723@studenti.univpm.it>
2020-03-14 19:04:55 +0100

    Commit JsonParser String -> ArrayList

--- Project/src/main/java/it/univpm/Project/utils/JsonParser.java
+++ Project/src/main/java/it/univpm/Project/utils/JsonParser.java
@@ -2,29 +2,23 @@ package it.univpm.Project.utils;
 
 import it.univpm.Project.dataset.Data;
 import it.univpm.Project.dataset.Funding;
-
-import java.io.File;
-import java.io.FileInputStream;
+import it.univpm.Project.dataset.Data;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonParser {
 	
-	private FileReader reader;
 	private String data;
 	private String[] line;
 	private ArrayList<Funding> af;
+	ObjectMapper mapper = new ObjectMapper();
 	
 	public JsonParser(String data)
 	{
@@ -33,20 +27,20 @@ public class JsonParser {
 	}
 	
 	public void parse() throws FileNotFoundException, IOException, NullPointerException, NumberFormatException {
-		File jsonInputFile = new File("dataFile.json");
-        InputStream is;
-        is = new FileInputStream(jsonInputFile);
-        
-        // Create JsonReader from Json.
-        JsonReader reader = Json.createReader(is);
-        
-        // Get the JsonObject structure from JsonReader.
-        JsonObject empObj = reader.readObject();
-        
-        while ((line = reader.read()) != null)
-        {
-        	af.add(new Funding(line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], data, data, data, data, data, data, Float.parseFloat(line[8]), 0, 0, 0, 0, 0));
-        }
-        reader.close();
-	}
+		String input;
+		ArrayList<String> text = new ArrayList<String>();
+		try {
+		    BufferedReader reader = new BufferedReader(new FileReader("dataFile.json"));
+		    for (input = reader.readLine(); input != null; input = reader.readLine()) {
+		         text.add(input);
+		    }
+		    ArrayList<Funding> af = (ArrayList<Funding>) Arrays.asList(mapper.readValue(input, arrFunding));
+		    reader.close();
+		} catch (FileNotFoundException fnfe) {
+		    fnfe.printStackTrace();
+		} catch(IOException ioe){
+		    ioe.printStackTrace();
+		}
+		
+    	}
 }
