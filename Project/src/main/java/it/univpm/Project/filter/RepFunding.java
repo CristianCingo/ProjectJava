374741ae532d567ef52d15b117395d14a3c2bfcc
Christian <s1084723@studenti.univpm.it>
2020-03-16 16:16:04 +0100

    Push

--- Project/src/main/java/it/univpm/Project/filter/RepFunding.java
+++ Project/src/main/java/it/univpm/Project/filter/RepFunding.java
@@ -1,25 +1,20 @@
 package it.univpm.Project.filter;
 
-import java.io.FileInputStream;
-import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonIOException;
-import com.google.gson.stream.JsonReader;
 
 import it.univpm.Project.dataset.Funding;
 import it.univpm.Project.filter.RepInterface;
 
 
-public class RepFunding<ArrayFunding> implements RepInterface<Funding>{
+public class RepFunding implements RepInterface<Funding>{
 	
-	private static final String filename ="C:\\\\fileName.json";  
+	private static final String filename ="C://dataFile.json";  
 	private static List<Funding> FundingList=new ArrayList<Funding>();
 	private static Filter<Funding> filterService=new Filter<Funding>(); 
 	
@@ -28,19 +23,22 @@ public class RepFunding<ArrayFunding> implements RepInterface<Funding>{
 	/**
 	 * Il costruttore effettua un parsing dei dati all'interno del json in data object
 	 * mediante l'utilizzo della libreria univocity-parser json: viene popolata la collection hotelList.
-	 * @throws IOException 
-	 * @throws JsonIOException 
 	 */
-	public RepFunding()
+	public RepFunding() 
 	{
-		try {
-		Gson gson = new Gson();
+	        Gson gson = new Gson();
 
-		// 1. Java object to JSON file
-		gson.toJson(FundingList, new FileWriter(filename));
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
+	        try (Reader reader = new FileReader(filename)) {
+
+	            // Converte JSON File in Java Object
+	            Funding f = gson.fromJson(reader, Funding.class);
+				
+				// stampa
+	            System.out.println(f);
+
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
 	}
 	
 	//region method not used
